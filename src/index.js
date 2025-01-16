8bccfcd1f962266925342a8425c0efdf370eba73
Bulus Hamnu <hamnubulus@gmail.com>
2025-01-16 16:25:10 +0100

    refine chart responsiness and minor editing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 /* Dashboard scrpt */
 const { Calendar } = window.VanillaCalendarPro;
 const taskChart = document.getElementById('task-chart');
+const canvas = document.querySelector('canvas'); 
+let aspectRatio;
+let dataset = [12, 19, 5, 15]
+let datasetPercents = ['12%', '19%', '5%', '15%'];
 
 
 
@@ -86,32 +90,56 @@ calendar.init();
 
 
 /* Initiaciating TaskChart */
+let chart;
 
-new Chart(taskChart, {
-  type: 'bar',
-  data: {
-    labels: ['12%', '19%', '5%', '15%'],
-    datasets: [{
-      label: 'Percentage of tasks',
-      data: [12, 19, 5, 15],
-      backgroundColor: [
-        'rgba(255, 204, 0, 1)',
-        'rgba(34, 255, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(197, 163, 163, 1)'
-      ],
-      borderWidth: 1,
-      responsive : true,
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+window.addEventListener("resize", () => {
+  if(window.matchMedia("(max-width: 576px)").matches) {
+    aspectRatio = 2/1.5;
+  } else {
+    aspectRatio = 2/1
+  }
+
+
+  chart.destroy();
+  createChart()
+})
+
+
+
+function createChart () {
+  canvas.width = canvas.parentElement.clientWidth;
+  canvas.height = canvas.parentElement.clientWidth/aspectRatio;
+  
+
+  chart = new Chart(taskChart, {
+    type: 'bar',
+    data: {
+      labels: datasetPercents.map(d => {return d}),
+      datasets: [{
+        label: 'Rate of tasks',
+        data: dataset.map((d) => { return d}),
+        backgroundColor: [
+          'rgba(255, 204, 0, 1)',
+          'rgba(34, 255, 0, 1)',
+          'rgba(255, 0, 0, 1)',
+          'rgba(197, 163, 163, 1)'
+        ],
+        borderWidth: 1,
+        responsive : true,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
+  });
+}
+
+
+createChart ()
 
 
 
