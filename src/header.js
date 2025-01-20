eab066e76f7f98ecff5e5572c5081247582a39bf
Bulus Hamnu <hamnubulus@gmail.com>
2025-01-20 16:44:47 +0100

    created the theme changing functionality and add the script at top of all page some the element load with the right colors

--- src/header.js
+++ src/header.js
@@ -1,9 +1,28 @@
 /* menu and header code */
 const themeSwitch = document.querySelector('.theme-switch');
 
+
+
+
+// Dark Mode Switcher
+if(body.classList.contains("dark-theme")) {
+    themeSwitch.classList.add("dark-mode")
+} else {
+    themeSwitch.classList.remove("dark-mode")
+}
+
 themeSwitch.addEventListener("click", () => {
-    document.body.classList.toggle('dark-theme');
-    
-    // localStorage.setItem('theme', document.body.classList.contains('dark-theme')? 'dark' : 'light');
+    if(body.classList.contains("dark-theme")) {
+        localStorage.setItem("demy-tasks-theme", "light");
+        body.classList.remove("dark-theme");
+        themeSwitch.classList.remove("dark-mode")
+
+    } else {
+        localStorage.setItem("demy-tasks-theme", "dark");
+        body.classList.add("dark-theme");
+        themeSwitch.classList.add("dark-mode")
+
+    }
+
 })
 
