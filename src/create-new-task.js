b4d21175113f94170bb8d5207b97c35f18399744
Bulus Hamnu <hamnubulus@gmail.com>
2025-01-20 18:33:26 +0100

    remove the vanilla calander and add normal calander date input

--- src/create-new-task.js
+++ src/create-new-task.js
@@ -1,5 +1,4 @@
 /*  page creation script*/
-const { Calendar } = window.VanillaCalendarPro;
 const addReminderToogle = document.getElementById("yes")
 const removeReminderToogle = document.getElementById("no")
 const reminderOptionConst = document.querySelector(".reminder-selector")
@@ -34,26 +33,6 @@ removeReminderToogle.addEventListener("click", () => {
 
 
 
-
-/* Clander script for reminder date and time */
-const options = {
-    inputMode: true,
-    positionToInput: 'auto',
-    onChangeToInput: function (self) {
-      if (!self.context.inputElement) return;
-      if (self.context.selectedDates[0]) {
-        self.context.inputElement.value = self.context.selectedDates[0];
-        
-        self.hide();
-      } else {
-        self.context.inputElement.value = '';
-      }
-    },
-  };
-  
-  
-const calendarInput = new Calendar('#reminder-calander', options);
-calendarInput.init();
 
 
 
